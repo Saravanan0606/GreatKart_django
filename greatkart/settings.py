ee8d5486124a02ba5fe7933963b0a5ae2947bf5d
Saravanapriyan T <saravananramya0606@gmail.com>
2023-10-12 16:29:21 +0530

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -138,3 +138,16 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR /'media'
+
+
+from django.contrib.messages import constants as messages
+MESSAGES_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# SMTP CONFIGURATION
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'saravanan06.developer@gmail.com'
+EMAIL_HOST_PASSWORD = 'vddi bzze zuxs mawb'
+EMAIL_USE_TLS = True
